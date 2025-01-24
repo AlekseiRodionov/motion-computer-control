b7dd38621d193d7123740e623c74924bb264610b
AlekseiRodionov <aleksei.rodionov-work@yandex.ru>
2025-01-25 00:54:00 +0300

    Implemented wrapper class GestureDetector for YOLO and SSDLite models (without training, only prediction).

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
-import numpy as np
+from GestureDetector import GestureDetector
 import cv2
-import torch
-import ultralytics
+
+model = GestureDetector(model_type='YOLO', path_to_file='YOLOv10n_gestures.pt')
+model2 = GestureDetector(model_type='SSDLite', path_to_file='SSDLiteMobileNetV3Large.pth')
+camera = cv2.VideoCapture(0)
+
+while True:
+    _, image = camera.read()
+    image = cv2.flip(image, 1)
+    result = model.predict(image)
+    result2 = model2.predict(image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+del camera
