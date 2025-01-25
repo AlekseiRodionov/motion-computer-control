d355a42dfe761fb078322dab26bd4c508e4a113e
AlekseiRodionov <aleksei.rodionov-work@yandex.ru>
2025-01-25 19:22:05 +0300

    Added checkpoints for YOLO and SSDLite models, removed cv2.imshow from test.py

--- test.py
+++ test.py
@@ -1,16 +1,13 @@
 from GestureDetector import GestureDetector
 import cv2
 
-model = GestureDetector(model_type='YOLO', path_to_file='YOLOv10n_gestures.pt')
+model1 = GestureDetector(model_type='YOLO', path_to_file='YOLOv10n_gestures.pt')
 model2 = GestureDetector(model_type='SSDLite', path_to_file='SSDLiteMobileNetV3Large.pth')
 camera = cv2.VideoCapture(0)
 
-while True:
-    _, image = camera.read()
-    image = cv2.flip(image, 1)
-    result = model.predict(image)
-    result2 = model2.predict(image)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+_, image = camera.read()
+image = cv2.flip(image, 1)
+print('model 1 result:\n ', model1.predict(image))
+print('model 2 result:\n ', model2.predict(image))
 
 del camera
