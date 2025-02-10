7801df23e322e7477904990e24e0b558c654ae21
AlekseiRodionov <aleksei.rodionov-work@yandex.ru>
2025-02-10 23:53:08 +0300

    CommandExecutor class has been changed. Loading data from JSON files has been changed.

--- CommandExecutor.py
+++ CommandExecutor.py
@@ -4,39 +4,38 @@ import json
 
 class CommandExecutor():
 
-    def __init__(self, path_to_commands_file='commands.json'):
-        self.path_to_commands_file = path_to_commands_file
-        self.commands_dict = self.__import_commands_dict()
+    def __init__(self):
+        self.commands_dict = dict()
         self.command_variables = dict()
 
-    def __import_commands_dict(self):
-        if not os.path.isfile(self.path_to_commands_file):
-            return dict()
-        with open(self.path_to_commands_file, 'r') as commands_file:
-            commands_dict = json.load(commands_file)
-        return commands_dict
+    def load_commands_dict(self, path_to_commands_file):
+        with open(path_to_commands_file, 'r', encoding='utf8') as commands_file:
+            data = commands_file.read()
+            if data:
+                self.commands_dict = json.loads(data)
+            else:
+                self.commands_dict = dict()
+        return self.commands_dict
 
-    def create_command(self, gesture_seq=None, algorithm_filename=None):
-        self.commands_dict[str(gesture_seq)] = algorithm_filename
-        with open(self.path_to_commands_file, 'w') as commands_file:
+    def create_command(self, path_to_commands_file, gesture=None, algorithm_filename=None):
+        self.commands_dict[str(gesture)] = algorithm_filename
+        with open(path_to_commands_file, 'w', encoding='utf8') as commands_file:
+            json.dump(self.commands_dict, commands_file)
+    
+    def delete_command(self, path_to_commands_file, gesture):
+        with open(path_to_commands_file, 'r', encoding='utf8') as commands_file:
+            data = commands_file.read()
+            if data:
+                self.commands_dict = json.loads(data)
+                self.commands_dict.pop(str(gesture))
+        with open(path_to_commands_file, 'w', encoding='utf8') as commands_file:
             json.dump(self.commands_dict, commands_file)
 
-    def __command_filtering(self, gesture_seq=None):
-        while len(gesture_seq) > 0:
-            str_gesture_seq = str(gesture_seq)
-            if self.commands_dict.get(str_gesture_seq) is None:
-                gesture_seq.pop(0)
-            else:
-                return self.commands_dict[str_gesture_seq]
-        return None
-
-    def execute_command(self, gesture_seq=None, coords_seq=None):
-        # gesture_seq - это кортеж, внутри которого находятся классы жестов.
-        # В будущем нужно будет придумать, как к этому добавить ещё координаты.
-        algorithm_filename = self.__command_filtering(gesture_seq)
+    def execute_command(self, gesture=None, coords=None):
+        algorithm_filename = self.commands_dict.get(str(gesture))
         if algorithm_filename is not None:
             algorithm = SourceFileLoader('algorithm', algorithm_filename).load_module()
-            self.command_variables = algorithm.main(gesture_seq, coords_seq, self.command_variables)
+            self.command_variables = algorithm.main(gesture, coords, self.command_variables)
 
 
 
