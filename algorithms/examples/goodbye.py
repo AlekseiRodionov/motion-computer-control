e764bae612cf557f3aea7db2b73bb1682e236356
AlekseiRodionov <aleksei.rodionov-work@yandex.ru>
2025-02-25 18:52:58 +0300

    Removed files test_commands.json and test_commands2.json.

--- algorithms/examples/goodbye.py
+++ algorithms/examples/goodbye.py
@@ -3,4 +3,8 @@ import os
 def main(gesture_seq, coords_seq, command_executor):
     if not command_executor.commands_variables.get('is_blocked', False):
         print('goodbye!')
-        command_executor.load_commands_dict(os.path.join('algorithms', 'examples', 'test_commands.json'))
+        try:
+        	# A new command dictionary can be loaded in a similar manner.
+        	command_executor.load_commands_dict(os.path.join('algorithms', 'examples', 'test_commands.json'))
+        except:
+        	pass
--- algorithms/examples/hello.py
+++ algorithms/examples/hello.py
@@ -3,4 +3,8 @@ import os
 def main(gesture_seq, coords_seq, command_executor):
     if not command_executor.commands_variables.get('is_blocked', False):
         print('hello!')
-        command_executor.load_commands_dict(os.path.join('algorithms', 'examples', 'test_commands2.json'))
+        try:
+        	# A new command dictionary can be loaded in a similar manner.
+        	command_executor.load_commands_dict(os.path.join('algorithms', 'examples', 'test_commands2.json'))
+        except:
+        	pass
